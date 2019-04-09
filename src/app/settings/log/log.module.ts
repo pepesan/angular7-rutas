cc41e1bc4b411ac487cbf12b9cb5c33f63586a96
pepesan <pepesan@gmailcom>
2019-04-09 20:33:16 +0200

    mejoras en log

--- src/app/settings/log/log.module.ts
+++ src/app/settings/log/log.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ListLogComponent } from './list-log/list-log.component';
 import {LOG_ROUTES} from './log.route';
 import {RouterModule} from '@angular/router';
+import { DeleteLogComponent } from './delete-log/delete-log.component';
 
 @NgModule({
-  declarations: [ListLogComponent],
+  declarations: [ListLogComponent, DeleteLogComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
       {
-        path: 'settings/log',
+        path: 'settings/log', // prefijo de rutas
         children: LOG_ROUTES
       }
     ])
